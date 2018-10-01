a229f15d32d3e91674703dfa158a488d9ab29d24
rocky <ruther.bergonia@gmail.com>
2018-10-01 22:23:36 +0800

    added iView css

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <Menu mode="horizontal" :theme="theme1" active-name="1">
+      <MenuItem name="1">
+        Charting
+      </MenuItem>
+    </Menu>
     <router-view/>
   </div>
 </template>
@@ -27,3 +28,12 @@
   }
 }
 </style>
+<script>
+    export default {
+        data () {
+            return {
+                theme1: 'dark'
+            }
+        }
+    }
+</script>
